8900f7226bf5b08f607a0651be41d4d3509c011d
George McCadden III <thethirdone.g@gmail.com>
2020-10-05 11:09:07 -0700

    Giving the seperate blocks attributes

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Problem Domain :
 
-// Building the game logic for the game Minesweeper
+// Building the game logic for the game Minesweeper. The board is a rectangle and its length and height can be determined by the player. The mines are created at random.
+
+function Block(row, column, mine, surroundingMines) {
+    return {
+        coordinates: row + '' + column,
+        row: row,
+        column: column,
+        mine: true,
+        surroundingMines: surroundingMines,
+    }
+}
